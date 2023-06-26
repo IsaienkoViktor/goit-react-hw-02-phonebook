11eff8363fbe709ef052290ecd414c1e8fe0dee7
IsaienkoViktor <isaenkov1986@gmail.com>
2023-06-26 20:42:14 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,90 @@
 import { Component } from 'react';
-import { ContactName } from './ContactName/ContactName';
+import shortid from 'shortid';
+import Container from './Container/Container.js';
+import ContactForm from './ContactForm/ContactForm.js';
+import Filter from './Filter/Filter.js';
+import ContactList from './ContactList/ContactList.js';
 
-export default class App extends Component {
+class App extends Component {
   state = {
-    contacts: [],
-    name: '',
+    contacts: [
+      { id: shortid.generate(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: shortid.generate(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: shortid.generate(), name: 'Eden Clements', number: '645-17-79' },
+      { id: shortid.generate(), name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  handleChangeInputDate = title => {
-    this.setState({ name: title });
-    console.log(title);
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+
+    const { contacts } = this.state;
+
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts.`);
+    } else if (contacts.find(contact => contact.number === number)) {
+      alert(`${number} is already in contacts.`);
+    } else if (name.trim() === '' || number.trim() === '') {
+      alert("Enter the contact's name and number phone!");
+    } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(number)) {
+      alert('Enter the correct number phone!');
+    } else {
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+    }
   };
-  handleAddItem = title => {
-    console.log(title);
-    this.setState(prevState => ({
-      contacts: [
-        ...prevState.contacts,
-        { name: title, id: crypto.randomUUID() },
-      ],
+
+  deleteContact = contactId => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   render() {
-    const { contacts, name } = this.state;
+    const { filter, contacts } = this.state;
+    const visibleContacts = this.getVisibleContacts();
     return (
-      <>
-        <header>
-          <div>Logo</div>
-        </header>
-        <ContactName
-          onAddItem={this.handleAddItem}
-          inputValue={name}
-          onChangeData={this.handleChangeInputDate}
-          contacts={contacts}
-          title="Contacts"
-        />
-      </>
+      <Container>
+        <h1>PhoneBook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        {contacts.length > 1 && (
+          <Filter value={filter} onChange={this.changeFilter} />
+        )}
+        {contacts.length > 0 ? (
+          <ContactList
+            contacts={visibleContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        ) : (
+          <p>Your phonebook is empty. Please add contact.</p>
+        )}
+      </Container>
     );
   }
 }
 
-// const wrapper = styled.div`
-//   padding: 15px 40px;
-// `;
+export default App;
